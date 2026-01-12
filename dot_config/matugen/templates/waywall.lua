7435ed2a1a62f618ce8ce431f5c5dff080cbb8b5
inlinefun <inlinefun@proton.me>
2026-01-12 18:18:52 +0530

    update(matugen): waywall colors

--- dot_config/matugen/templates/waywall.lua
+++ dot_config/matugen/templates/waywall.lua
@@ -1,3 +1,6 @@
 return {
-    background = "{{colors.surface.default.hex}}"
+    background = "{{colors.surface.default.hex}}",
+    primary = "{{colors.primary.default.hex}}",
+    secondary = "{{colors.secondary.default.hex}}",
+    text = "{{colors.on_surface.default.hex}}"
 }
