79bb65a2461705bea589b11fcd3b08ca7f35fe1f
inlinefun <inlinefun@proton.me>
2026-02-22 17:17:42 +0530

    update(zsh): $PATH

--- home/dot_config/zsh/00-path.zsh
+++ home/dot_config/zsh/00-path.zsh
@@ -2,5 +2,7 @@ typeset -U path
 path=(
   "$HOME/.deno/bin"             # add deno programs
   "/usr/lib/qt6/bin"            # qt programs, ex: svgtoqml
+  "$HOME/flutter/bin"           # flutter
+  "$HOME/Android/Sdk/"          # android sdk
   $path
 )
